f5152fc5db4771be1e8e3325c344ac6f1a37ca02
Bogdan <bogdan.plockiy@mail.ru>
2023-10-03 23:52:19 +0300

    Добавьте файлы проекта.

---
+++ Mai_3course_1sem_SFMLbilliard/Mai_3course_1sem_SFMLbilliard.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(VideoMode(400, 400), L"Бильярд");
+
+	window.setVerticalSyncEnabled(true);
+
+	CircleShape shape(100.f, 64);
+	shape.setPosition(100, 100);
+	shape.setFillColor(Color::Magenta);
+	shape.setRotation(45);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear(Color::Blue);
+		window.draw(shape);
+		window.display();
+	}
+}
